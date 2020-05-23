7fe21c506ae81edc9981a44ac5aa927712cb3bd0
jose <jose@airspacetechnologies.com>
2020-05-23 00:26:12 -0700

    more docs

--- parsers/int64.go
+++ parsers/int64.go
@@ -4,11 +4,14 @@ import (
 	"strconv"
 )
 
+// Int64 is the env.Parser implementation for the int64 type.
 type Int64 struct {
 	Pointer *int64
 	Default int64
 }
 
+// NewInt64 returns an Int64 struct as value which holds a pointer
+// and a default value.
 func NewInt64(ptr *int64, def int64) Int64 {
 	return Int64{
 		Pointer: ptr,
@@ -16,6 +19,9 @@ func NewInt64(ptr *int64, def int64) Int64 {
 	}
 }
 
+// Parse converts the string to an int64 using strconv.ParseInt
+// and returns an error if that fails. Otherwise it sets the value
+// of the pointer to the result of the conversion.
 func (i Int64) Parse(str string) error {
 	conv, err := strconv.ParseInt(str, 10, 64)
 	if err != nil {
@@ -26,10 +32,13 @@ func (i Int64) Parse(str string) error {
 	return err
 }
 
+// SetToDefault sets the value of the pointer to the default.
 func (i Int64) SetToDefault() {
 	*i.Pointer = i.Default
 }
 
+// Value returns the value of the pointer or nil as
+// an interface{}.
 func (i Int64) Value() interface{} {
 	if i.Pointer == nil {
 		return nil
--- parsers/string.go
+++ parsers/string.go
@@ -1,10 +1,13 @@
 package parsers
 
+// String is the env.Parser implementation for the string type.
 type String struct {
 	Pointer *string
 	Default string
 }
 
+// NewString returns a String struct as value which holds a pointer
+// and a default value.
 func NewString(ptr *string, def string) String {
 	return String{
 		Pointer: ptr,
@@ -12,15 +15,21 @@ func NewString(ptr *string, def string) String {
 	}
 }
 
+// Parse sets the value of the pointer to the string. It does not
+// fail or convert since the value is already a string. The error
+// returned will always be nil.
 func (s String) Parse(str string) error {
 	*s.Pointer = str
 	return nil
 }
 
+// SetToDefault sets the value of the pointer to the default.
 func (s String) SetToDefault() {
 	*s.Pointer = s.Default
 }
 
+// Value returns the value of the pointer or nil as
+// an interface{}.
 func (s String) Value() interface{} {
 	if s.Pointer == nil {
 		return nil
